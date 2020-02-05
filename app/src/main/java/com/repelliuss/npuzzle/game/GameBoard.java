cf4c83967df57dd96bf7541b6c3a556001b60038
repelliuss <officialrepelliuss@gmail.com>
2020-02-05 23:19:35 +0300

    Modified GameBoard interface

--- app/src/main/java/com/repelliuss/npuzzle/game/GameBoard.java
+++ app/src/main/java/com/repelliuss/npuzzle/game/GameBoard.java
@@ -1,11 +1,13 @@
 package com.repelliuss.npuzzle.game;
 
+import com.repelliuss.npuzzle.utilities.Index2D;
+
 public interface GameBoard<T> {
 
     int getRow();
     int getColumn();
-    T getCell();
+    T getCell(Index2D position);
     void setRow(int argColumn);
     void setColumn(int argColumn);
-    void setCell(int yPos, int xPos, T value);
+    void setCell(Index2D position, T value);
 }
