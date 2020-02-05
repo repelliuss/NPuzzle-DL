bcdfd40a7ee1b970aedeb3a156f0d8003cdb8595
repelliuss <officialrepelliuss@gmail.com>
2020-02-05 23:53:04 +0300

    Added some arithmetic operations to Index2D

--- app/src/main/java/com/repelliuss/npuzzle/utilities/Index2D.java
+++ app/src/main/java/com/repelliuss/npuzzle/utilities/Index2D.java
@@ -1,19 +1,35 @@
 package com.repelliuss.npuzzle.utilities;
 
 public final class Index2D {
-    private final int yPos;
-    private final int xPos;
+    private int y;
+    private int x;
 
-    public Index2D(int argYPos, int argXPos) {
-        yPos = argYPos;
-        xPos = argXPos;
+    public Index2D(int argY, int argX) {
+        y = argY;
+        x = argX;
     }
 
-    public int getYPos() {
-        return yPos;
+    public int getY() {
+        return y;
     }
 
-    public int getXPos() {
-        return xPos;
+    public int getX() {
+        return x;
+    }
+
+    public void incX() {
+        ++x;
+    }
+
+    public void incY() {
+        ++y;
+    }
+
+    public void decX() {
+        --x;
+    }
+
+    public void decY() {
+        --y;
     }
 }
