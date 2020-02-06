f10dd8f8385b5ae5758b3ef8ba2443470901c440
repelliuss <officialrepelliuss@gmail.com>
2020-02-06 22:33:51 +0300

    Implemented Puzzle interface

--- app/src/main/java/com/repelliuss/npuzzle/game/Puzzle.java
+++ app/src/main/java/com/repelliuss/npuzzle/game/Puzzle.java
@@ -1,4 +1,17 @@
 package com.repelliuss.npuzzle.game;
 
-public interface Puzzle extends GameBoard {
+import androidx.annotation.NonNull;
+
+import com.repelliuss.npuzzle.utilities.Move;
+
+public interface Puzzle<E extends BoardPiece> extends GameBoard<E> {
+
+    Move getLastMove();
+    int getMoveCount();
+    boolean move(@NonNull final Move move);
+    boolean checkMove(@NonNull final Move move);
+    boolean checkMove(int yMove, int xMove);
+    void reset();
+    boolean isSolved();
+
 }
