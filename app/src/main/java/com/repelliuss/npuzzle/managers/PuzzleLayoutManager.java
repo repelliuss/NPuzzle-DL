81d734f38e8de0ea4ea7fd3687ea90d0dace5a9c
repelliuss <officialrepelliuss@gmail.com>
2020-02-08 02:31:53 +0300

    Added custom layout manager

---
+++ app/src/main/java/com/repelliuss/npuzzle/managers/PuzzleLayoutManager.java
@@ -0,0 +1,19 @@
+package com.repelliuss.npuzzle.managers;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.GridLayoutManager;
+
+public class PuzzleLayoutManager extends GridLayoutManager {
+
+    private boolean scrollable = false;
+
+    public PuzzleLayoutManager(Context context, int columnCount) {
+        super(context, columnCount);
+    }
+
+    @Override
+    public boolean canScrollVertically() {
+        return scrollable;
+    }
+}
