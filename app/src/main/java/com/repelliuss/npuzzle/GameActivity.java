4e506bbb7ea925d8739961af0199450eea17ca51
repelliuss <officialrepelliuss@gmail.com>
2020-02-08 03:54:57 +0300

    Modified sample GameActivity

--- app/src/main/java/com/repelliuss/npuzzle/GameActivity.java
+++ app/src/main/java/com/repelliuss/npuzzle/GameActivity.java
@@ -1,26 +1,32 @@
 package com.repelliuss.npuzzle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.repelliuss.npuzzle.R;
-import com.repelliuss.npuzzle.adapters.RecyclerViewAdapter;
+import com.repelliuss.npuzzle.adapters.SlidePuzzleAdapter;
+import com.repelliuss.npuzzle.game.Move;
+import com.repelliuss.npuzzle.game.NPuzzle;
+import com.repelliuss.npuzzle.managers.PuzzleLayoutManager;
 
 public class GameActivity extends AppCompatActivity {
 
-    private RecyclerViewAdapter adapter;
-    private String[] data = { "1", "2", "3", "4", "5", "6", "7", "8"};
+    private SlidePuzzleAdapter<Integer> adapter;
     private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        NPuzzle puzzle = new NPuzzle(9, 3);
+        puzzle.move(Move.UP);
+        puzzle.move(Move.LEFT);
+        puzzle.reset();
+
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
@@ -41,13 +47,9 @@ public class GameActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.rv_game_area);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 3 ) {
-            @Override
-            public boolean canScrollVertically() {
-                return false;
-            }
-        });
-        adapter = new RecyclerViewAdapter(this, data);
+        recyclerView.setLayoutManager(new PuzzleLayoutManager(this, 3 ));
+        adapter = new SlidePuzzleAdapter<>(this, puzzle);
         recyclerView.setAdapter(adapter);
+        puzzle.move(Move.LEFT);
     }
 }
