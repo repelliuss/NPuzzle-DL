bd0393be72ade53b6356fbede65121a3c42198d7
repelliuss <officialrepelliuss@gmail.com>
2020-02-10 22:24:00 +0300

    Implemented overall GameActivity
    GameActivity will be place where NPuzzle is playable

--- app/src/main/java/com/repelliuss/npuzzle/GameActivity.java
+++ app/src/main/java/com/repelliuss/npuzzle/GameActivity.java
@@ -1,22 +1,31 @@
 package com.repelliuss.npuzzle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
+import com.repelliuss.npuzzle.game.Puzzle;
+import com.repelliuss.npuzzle.game.SlidePuzzle;
+import com.repelliuss.npuzzle.ui.OnSwipeListener;
 import com.repelliuss.npuzzle.ui.PuzzlePieceDecoration;
 import com.repelliuss.npuzzle.ui.SlidePuzzleAdapter;
 import com.repelliuss.npuzzle.game.NPuzzle;
 import com.repelliuss.npuzzle.ui.PuzzleLayoutManager;
+import com.repelliuss.npuzzle.ui.SlidePuzzleSwipeListener;
 import com.repelliuss.npuzzle.utils.Screen;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity
+    implements View.OnTouchListener {
 
     private SlidePuzzleAdapter<Integer> adapter;
     private RecyclerView recyclerView;
-    private NPuzzle puzzle;
+    private Puzzle<SlidePuzzle<Integer>.Piece> puzzle;
+    private GestureDetectorCompat detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +42,18 @@ public class GameActivity extends AppCompatActivity {
         }
         else puzzle = new NPuzzle(3, 3);
 
-        adapter = new SlidePuzzleAdapter<>(this, puzzle);
+        adapter = new SlidePuzzleAdapter<>(this, (NPuzzle) puzzle);
+
+        activateSwipe();
 
         recyclerView = findViewById(R.id.rv_game_area);
         configurePuzzleView();
+    }
 
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        view.performClick();
+        return detector.onTouchEvent(motionEvent);
     }
 
     private void configurePuzzleView() {
@@ -49,5 +65,14 @@ public class GameActivity extends AppCompatActivity {
                 Screen.dpToPixel(-4, this)));
 
         recyclerView.setAdapter(adapter);
+    }
+
+    private void activateSwipe() {
+
+        View swipeable = findViewById(R.id.v_swipeable);
+        OnSwipeListener swipeListener =
+                new SlidePuzzleSwipeListener<>((SlidePuzzle<Integer>) puzzle, adapter);
+        detector = new GestureDetectorCompat(this, swipeListener);
+        swipeable.setOnTouchListener(this);
     }
 }
