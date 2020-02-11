7fa90db62704e1e7331c2e29fdbe3b6facfeb0fa
repelliuss <officialrepelliuss@gmail.com>
2020-02-11 19:28:01 +0300

    Modified GameBoard to extend Game interface

--- app/src/main/java/com/repelliuss/npuzzle/game/GameBoard.java
+++ app/src/main/java/com/repelliuss/npuzzle/game/GameBoard.java
@@ -2,7 +2,7 @@ package com.repelliuss.npuzzle.game;
 
 import com.repelliuss.npuzzle.utils.Index2D;
 
-public interface GameBoard<E extends BoardPiece> {
+public interface GameBoard<E extends BoardPiece> extends Game {
     int getRow();
     int getColumn();
     E getPiece(Index2D pos);
