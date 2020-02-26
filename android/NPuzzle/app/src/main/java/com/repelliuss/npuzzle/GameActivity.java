e65abdc31acaa863cbee88001080fef4178a7148
repelliuss <officialrepelliuss@gmail.com>
2020-02-26 18:07:13 +0300

    Implemented game finish event

--- android/NPuzzle/app/src/main/java/com/repelliuss/npuzzle/GameActivity.java
+++ android/NPuzzle/app/src/main/java/com/repelliuss/npuzzle/GameActivity.java
@@ -8,9 +8,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.repelliuss.npuzzle.game.Puzzle;
 import com.repelliuss.npuzzle.game.SlidePuzzle;
@@ -71,13 +75,41 @@ public class GameActivity extends AppCompatActivity
 
     @Override
     public void onGameFinish() {
-        Log.d("PUZZLE STATUS", "SOLVED");
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.info_puzzle_solved, null);
+        View parent = findViewById(R.id.v_swipeable);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(parent, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
         view.performClick();
         return detector.onTouchEvent(motionEvent);
+    }
+
+    public void onNewGameClick(View view) {
+        finish();
     }
 
     public void onHintClick(View view) {
