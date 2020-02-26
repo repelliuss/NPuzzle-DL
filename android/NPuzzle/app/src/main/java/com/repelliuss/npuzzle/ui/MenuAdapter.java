82234b2ea79124f955d289817952b5c809a3e3da
repelliuss <officialrepelliuss@gmail.com>
2020-02-26 22:02:18 +0300

    Added a MenuAdapter

--- android/NPuzzle/app/src/main/java/com/repelliuss/npuzzle/ui/MenuAdapter.java
+++ android/NPuzzle/app/src/main/java/com/repelliuss/npuzzle/ui/MenuAdapter.java
@@ -1,4 +1,54 @@
 package com.repelliuss.npuzzle.ui;
 
-public class MenuAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.repelliuss.npuzzle.R;
+
+public class MenuAdapter
+        extends RecyclerView.Adapter<MenuAdapter.ViewHolder> {
+
+    private LayoutInflater inflater;
+
+    public MenuAdapter(Context context) {
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.input_block, parent, false);
+        return new MenuAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.getTextView().setText(String.valueOf((position % 7) + 3));
+    }
+
+    @Override
+    public int getItemCount() {
+        return Integer.MAX_VALUE;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView number;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            number = itemView.findViewById(R.id.txt_input_number);
+        }
+
+        TextView getTextView() {
+            return number;
+        }
+    }
 }
